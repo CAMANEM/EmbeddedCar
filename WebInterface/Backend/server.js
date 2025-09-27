69568094168ea97a3394d422c28dfc5d16b5de95
Andres <joseandres216@gmail.com>
2025-09-26 22:45:03 -0600

    Deployment in LAN

--- WebInterface/Backend/server.js
+++ WebInterface/Backend/server.js
@@ -47,7 +47,7 @@ app.use('*', (req, res) => {
 });
 
 // Start server
-app.listen(PORT, () => {
+app.listen(PORT, '0.0.0.0',() => {
   console.log(`Server is running on port ${PORT}`);
   console.log(`Access the API at: http://localhost:${PORT}`);
 });
